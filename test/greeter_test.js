7f59195b33d3bc5a66d194dd7a8b23df46243346
mactkg <mactkg@gmail.com>
2021-08-10 16:06:30 +0900

    impl owner()

--- test/greeter_test.js
+++ test/greeter_test.js
@@ -1,17 +1,32 @@
 const GreeterContract = artifacts.require("Greeter");
+let greeter;
+beforeEach(async () => {
+    greeter = await GreeterContract.deployed();
+});
 
-contract("Greeter", () => {
+contract("Greeter", (accounts) => {
     it("has been deployed successfully", async () => {
-        const greeter = await GreeterContract.deployed();
         assert(greeter, "contract was not deployed");
     })
 
     describe("greet()", () => {
         it("returns 'Hello, World!'", async () => {
-            const greeter = await GreeterContract.deployed();
             const expected = "Hello, World!";
             const actual = await greeter.greet();
             assert.equal(actual, expected, "greeted with 'Hello, World!'");
+        });
+    });
+
+    describe('owner()', () => {
+        it('returns the address of the owner', async () => {
+            const owner = await greeter.owner();
+            assert(owner, "the current owner")
+        });
+
+        it('matches the address that originally deployed the contract', async() => {
+            const owner = await greeter.owner();
+            const expected = accounts[0];
+            assert.equal(owner, expected, "matches address used to deploy contract");
         });
     });
 });
@@ -19,7 +34,6 @@ contract("Greeter", () => {
 contract("Greeter: update greeting", () => {
     describe('setGreeting(string)', () => {
         it('sets greeting to passed in string', async () => {
-            const greeter = await GreeterContract.deployed()
             const expected = "Hi there!";
             await greeter.setGreeting(expected);
 
