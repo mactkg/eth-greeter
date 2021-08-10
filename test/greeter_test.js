5270e20c661f5b64cd5496ef1d4995d716b86c90
mactkg <mactkg@gmail.com>
2021-08-10 15:12:22 +0900

    4.3 - greeting

--- test/greeter_test.js
+++ test/greeter_test.js
@@ -5,4 +5,13 @@ contract("Greeter", () => {
         const greeter = await GreeterContract.deployed();
         assert(greeter, "contract was not deployed");
     })
+
+    describe("greet()", () => {
+        it("returns 'Hello, World!'", async () => {
+            const greeter = await GreeterContract.deployed();
+            const expected = "Hello, World!";
+            const actual = await greeter.greet();
+            assert.equal(actual, expected, "greeted with 'Hello, World!'");
+        });
+    });
 });
